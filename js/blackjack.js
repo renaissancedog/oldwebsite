29b8fbba9d3494f0ff1ead9bab1cb1bd16422b26
William <orangetnt827@gmail.com>
2024-09-26 15:08:47 -0500

    update blackjack

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,5 +1,8 @@
 let allCards=["2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","AS","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","AH",
 "2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","AD","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC","AC"]
+//FOR DEBUGGING BLACKJACKS
+// let allCards=["9S","10S","JS","QS","KS","AS","10H","JH","QH","KH","AH",
+// "10D","JD","QD","KD","AD","10C","JC","QC","KC","AC"]
 class Hand {
   constructor() {
     this.cards=[]
@@ -25,8 +28,7 @@ class Hand {
     if (this.score>21) {
       game.checkWin(true)
     } else if (this.score==21&&this.cards.length==2) {
-      alert("Blackjack") 
-      game.checkWin(false, true)
+      game.blackjack()
     }
   }
   clear() {
@@ -116,25 +118,28 @@ class Game {
         this.money-=this.amt
       }
     }
-    q("info").innerHTML=dealer.cardsToStr(dealer.cards, false)
-    q("playerCards").innerHTML=player.cardsToStr(player.cards)
-    q("info2").innerHTML="Dealer's Score: "+dealer.score
-    q("money").innerHTML="Money: $"+this.money
-    q("roundinfo").innerHTML="Round "+this.rounds
+    this.updateGameInfo() 
     this.newGame()
   }
   blackjack() {
     if (dealer.score==21) {
+      alert("Blackjack - Draw")
       q("info3").innerHTML="Draw"
     } else {
+      alert("Blackjack - Win")
       q("info3").innerHTML="Win"
       this.money+=1.5*this.amt
     }
-    q("info").innerHTML=dealer.cardsToStr(dealer.cards, false)
-    q("info2").innerHTML="Dealer's Score: "+dealer.score
+    this.updateGameInfo()
+    this.newGame()
+  }
+  updateGameInfo() {
+    q("dealerCards").innerHTML=dealer.cardsToStr(dealer.cards, false)
+    q("dealerScore").innerHTML="Dealer's Score: "+dealer.score
+    q("playerCards").innerHTML=player.cardsToStr(player.cards)
+    q("playerScore").innerHTML="Player's score: "+ player.score
     q("money").innerHTML="Money: $"+this.money
     q("roundinfo").innerHTML="Round "+this.rounds
-    this.newGame()
   }
   newGame() {
     if (game.rounds%5==0) {
